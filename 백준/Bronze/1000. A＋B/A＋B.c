7dbcf85470241c027f50f7b012aef13676c13a12
usergitgo <kjhjeff21@gmail.com>
2025-03-31 13:53:11 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    int y;
+    int sum;
+    
+    printf("");
+    scanf("%d", &x);
+    
+    printf("");
+    scanf("%d", &y);
+    
+    sum = x+y;
+    printf("%d", sum);
+    
+    return 0;
+    
+    
+}
